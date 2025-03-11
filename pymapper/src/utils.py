d5d677d6798490f3f235337afb8824302693ed6e
Julio Cordero <julioccorderoc@gmail.com>
2025-03-10 21:27:15 -0400

    Update types and improve docstrings

--- pymapper/src/utils.py
+++ pymapper/src/utils.py
@@ -1,19 +1,21 @@
 """
 utils.py
-==============
+========
 
-
+This module provides utility functions for serializing data and handling partial returns in the data mapping process.
+It includes functions to convert Pydantic models to JSON strings and to optionally serialize mapped data.
 
 """
 
-from typing import Dict, Union, Any
+from typing import Union, Any
 from json import dumps
 from pydantic import BaseModel
 
 from .exceptions import ObjectNotJsonSerializable
+from .types import DataMapped
 
 
-def serializer(object: Any) -> Union[Dict[str, Any], str]:
+def _serializer(object: Any) -> Union[DataMapped, str]:
     """Serializes a Pydantic model to a JSON string"""
     try:
         if isinstance(object, BaseModel):
@@ -25,21 +27,19 @@ def serializer(object: Any) -> Union[Dict[str, Any], str]:
 
 
 def partial_return(
-    mapped_data: Dict[str, Any], serialize: bool = False
-) -> Union[Dict[str, Any], str]:
-    """_summary_
+    mapped_data: DataMapped, serialize: bool = False
+) -> Union[DataMapped, str]:
+    """Returns mapped data, optionally serialized to a JSON string.
 
     Args:
-        mapped_data (Dict[str, Any]): _description_
-        serialize (bool, optional): _description_. Defaults to False.
+        mapped_data (DataMapped): The data to be returned, typically a dictionary of mapped values.
+        serialize (bool, optional): Flag indicating whether to serialize the data to a JSON string. Defaults to False.
 
     Returns:
-        Union[Dict[str, Any], str]: _description_
+        Union[DataMapped, str]: The mapped data as a dictionary if serialize is False, or as a JSON string if serialize is True.
     """
 
     if serialize:
-        serialized_data: str
-        serialized_data = dumps(mapped_data, indent=4, default=serializer)
-        return serialized_data
+        return dumps(mapped_data, indent=4, default=_serializer)
     else:
         return mapped_data
