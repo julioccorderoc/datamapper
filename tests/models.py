33ec685244a5293f0145b3936b9b04aa1ddba93e
Julio Cordero <julioccorderoc@gmail.com>
2025-03-19 19:27:38 -0400

    style imports

--- tests/models.py
+++ tests/models.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 from typing import List, Optional
-from pydantic import BaseModel, Field
+from pydantic import BaseModel
 from datetime import date
 
 # ---------------------------------------------------
